765e329c80b7298177bf7c2df144673070a7ab2c
ElyasAmri <elyas.amri@gmail.com>
2022-10-19 23:28:39 +0300

    working functionality

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-# import some image library
-from argparse import ArgumentParser, Namespace
+from PIL import Image
+from argparse import ArgumentParser
 
 ascii_chars = r'@#$%?*+;:,.'
 
 
-def main(args: Namespace):
-	path = args['file']
-	with open(path, 'r'):
-		# todo: get actual width
-		width = 0
-		# todo: get actual height
-		height = 0
-		# todo: replace i * j with get actual pixel from file
-		ascii_image = [[get_pixel_value(i * j) for i in range(width)] for j in range(height)]
+def main(args):
+	path = args.file
+	width = int(args.width)
+	height = int(args.height)
+	with Image.open(path) as image:
+		resized = resize_image(image, width)
+		grayified = resized.convert('L')
+		pixels = grayified.getdata()
+		ascii_image = [get_pixel_value(pixel) for pixel in pixels]
+	ascii_image = [ascii_image[i*width:(i+1)*width] for i in range(height)]
 
-	s = '\n'.join([''.join(i) for i in ascii_image])
-	print(s)
+	seq = '\n'.join([''.join(i) for i in ascii_image])
+
+	with open('result.txt', 'w') as out:
+		out.write(seq)
+
+
+
+def resize_image(image, new_width):
+	width, height = image.size
+	ratio = height / width
+	new_height = int(ratio * new_width)
+	resized_image = image.resize((new_width, new_height))
+	return resized_image
 
 
 def get_pixel_value(v):
-	return ascii_chars[v // 23]
+	return ascii_chars[v // 25]
 
 
 if __name__ == '__main__':
 	ap = ArgumentParser()
-	ap.add_argument('file', required=True)
-	ap.add_argument('width', default='400')
-	ap.add_argument('height', default='400')
+	ap.add_argument('file')
+	ap.add_argument('--width', default='400')
 
 	args = ap.parse_args()
 	main(args)
