b20b9edb7ffcbd65b6a77e442ca40890ee5c234d
ElyasAmri <elyas.amri@gmail.com>
2022-10-19 23:32:30 +0300

    add option to specify output file

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ ascii_chars = r'@#$%?*+;:,.'
 def main(args):
 	path = args.file
 	width = int(args.width)
-	height = int(args.height)
+	out_path = args.output
 	with Image.open(path) as image:
 		resized = resize_image(image, width)
 		grayified = resized.convert('L')
 		pixels = grayified.getdata()
 		ascii_image = [get_pixel_value(pixel) for pixel in pixels]
-	ascii_image = [ascii_image[i*width:(i+1)*width] for i in range(height)]
 
+	ascii_image = [ascii_image[i * width:(i + 1) * width] for i in range(grayified.height)]
 	seq = '\n'.join([''.join(i) for i in ascii_image])
 
-	with open('result.txt', 'w') as out:
+	with open(out_path, 'w') as out:
 		out.write(seq)
 
+	print(f'the result has been written to {out_path}')
 
 
 def resize_image(image, new_width):
@@ -38,6 +39,7 @@ if __name__ == '__main__':
 	ap = ArgumentParser()
 	ap.add_argument('file')
 	ap.add_argument('--width', default='400')
+	ap.add_argument('--output', default='result.txt')
 
 	args = ap.parse_args()
 	main(args)
